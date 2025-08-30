455124b01adcafe07da970243537a09860fa2d8e
Kunal Kamod <kunalkamod3@gmail.com>
2025-08-30 21:20:31 +0530

    Add files via upload

---
+++ ComOp.py
@@ -0,0 +1,8 @@
+a = 10
+b = 20
+print(f"Equalto: {a} == {b} = {a == b}")
+print(f"Greater than: {a} > {b} = {a > b}")
+print(f"Less than: {a} < {b} = {a < b}")
+print(f"Less than equal to: {a} <= {b} = {a <= b}")
+print(f"Greater than equal to: {a} >= {b} = {a >= b}")
+print(f"Not Equal to: {a} != {b} = {a != b}")
