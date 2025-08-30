14bb39332e23b0082c7228452788e367d36912a5
Kunal Kamod <kunalkamod3@gmail.com>
2025-08-30 21:19:36 +0530

    Add files via upload

---
+++ StringMethods.py
@@ -0,0 +1,6 @@
+message = "my name is kunal"
+print(f"Original name: {message}")
+print(f"Upper name: {message.upper()}")
+print(f"Lower name: {message.lower()}")
+print(f"title name: {message.title()}")
+print(f"new name: {message.replace('my', 'my self')}")  
