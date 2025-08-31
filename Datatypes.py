abc8ba0ccd374939f3421e4e466f4a7a2640aeac
kunal-kamod25 <kunalkamod3@gmail.com>
2025-08-31 19:31:20 +0530

    Data Types

---
+++ Datatypes.py
@@ -0,0 +1,11 @@
+name = "Kunal"
+age = 22
+height = 5.6
+isStudent = True
+RR=None
+
+print(f"Name: {name} Type: {type(name)}")
+print(f"Age: {age} Type: {type(age)}")
+print(f"Height: {height} Type: {type(height)}")
+print(f"isStudent: {isStudent} Type: {type(isStudent)}")
+print(f"None: {RR} Type: {type(RR)}")
