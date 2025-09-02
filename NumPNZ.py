ce24bf4fb225634769da44db152a38581eacc256
Kunal Kamod <kunalkamod3@gmail.com>
2025-09-02 22:37:49 +0530

    Check Number Is Positive , Negative Or Zero

---
+++ NumPNZ.py
@@ -0,0 +1,10 @@
+#Program To Check if a number is Positive , Negative Or Zero:
+
+Num = 12
+ 
+if(Num > 0):
+    print("The Number Is Positive")
+elif(Num == 0):
+    print("The Number Is Zero")
+else:
+    print("The Number Is Negative")
