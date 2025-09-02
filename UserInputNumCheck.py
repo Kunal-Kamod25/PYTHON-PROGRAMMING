a4232c199c16ca4841a2e256074ede3c83e5e57a
Kunal Kamod <kunalkamod3@gmail.com>
2025-09-02 22:43:25 +0530

    Check Num Pos Neg Zero User Input

---
+++ UserInputNumCheck.py
@@ -0,0 +1,8 @@
+Num = int(input("Enter the number : "))
+
+if(Num > 0):
+    print("The Number Is Positive")
+elif(Num == 0):
+    print("The Number is zero")
+else:
+    print("The Number is Negative")
