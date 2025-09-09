8ab5d842c4703f4caefa440f3fbcb1b6cb9561f7
Kunal Kamod <kunalkamod3@gmail.com>
2025-09-09 22:40:54 +0530

    Largest Of Three No If Elif

---
+++ LargestThreeNo.py
@@ -0,0 +1,7 @@
+Num = (input("Enter three numbers separated by space: ")).split()
+if (int(Num[0]) >= int(Num[1])) and (int(Num[0]) >= int(Num[2])):
+    print(f"Largest number is {Num[0]}")
+elif (int(Num[1]) >= int(Num[0])) and (int(Num[1]) >= int(Num[2])):
+    print(f"Largest number is {Num[1]}")
+else:
+    print(f"Largest number is {Num[2]}")
