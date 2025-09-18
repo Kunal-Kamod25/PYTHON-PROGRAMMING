09cab3383418ea69d4cf7a758165e6e7d60029dc
kunal-kamod25 <kunalkamod3@gmail.com>
2025-09-18 20:44:06 +0530

    Attempted 5 times and failed

---
+++ ForLoop/For6.py
@@ -0,0 +1,8 @@
+Successful = False
+for number in range(5):
+    print("Attempt")
+    if Successful:
+        print("Successful")
+        break
+else:
+        print("Attempted 5 times and Failed")
