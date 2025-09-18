f097a4f73a96b98410e50259c5fec3f75ce2ada2
kunal-kamod25 <kunalkamod3@gmail.com>
2025-09-18 20:35:12 +0530

    Resolve merge conflict in For3.py

--- ForLoop/For3.py
+++ ForLoop/For3.py
@@ -1,5 +1,11 @@
 Fruite = ["Banana" , "Apple" , "Mango" , "Orange"]
 for x in Fruite:
+<<<<<<< Updated upstream
     print(x)
     if x == "Apple":
         break
+=======
+    if x == "Mango":
+        continue
+    print(x)
+>>>>>>> Stashed changes
