631084a04d1cef01d5cf8e028c242cd11736b41f
Kunal Kamod <kunalkamod3@gmail.com>
2025-11-21 21:34:43 +0530

    Update Swapping.py

--- Python_Programs/Swapping.py
+++ Python_Programs/Swapping.py
@@ -1,8 +1,9 @@
-a = int(input("Enter first number: "))
-b = int(input("Enter second number: "))
+a = int(input("Enter First Number: "))
+b = int(input("Enter Second Number: "))
 
 print("Before swapping: a =", a, "b =", b)
 temp = a
 a = b
 b = temp
 print("After swapping: a =", a, "b =", b)
+
