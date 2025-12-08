edf3685271180bd6c5044760b9b3142e76ab42f1
Kunal Kamod <kunalkamod3@gmail.com>
2025-12-08 22:06:26 +0530

    CheckerBoard.py

---
+++ ForLoop/CheckerBoard.py
@@ -0,0 +1,13 @@
+n = int(input("Enter n: "))
+num = 1
+i = 1
+
+while i <= n:
+    for j in range(1, n + 1):
+        if (i + j) % 2 == 0:
+            print(num, end=" ")
+            num += 1
+        else:
+            print("- ", end="")
+    print()
+    i += 1
