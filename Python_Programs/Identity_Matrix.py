9eff6673543e7647ee991d356c09eaa945c40474
Kunal Kamod <kunalkamod3@gmail.com>
2025-12-10 20:32:11 +0530

    Create Identity_Matrix.py

---
+++ Python_Programs/Identity_Matrix.py
@@ -0,0 +1,24 @@
+r = int(input("Enter rows: "))
+c = int(input("Enter columns: "))
+
+print("Enter matrix elements:")
+A = [[0] * c for _ in range(r)]
+
+# Input matrix
+for i in range(r):
+    for j in range(c):
+        A[i][j] = int(input())
+
+# Convert to identity-style matrix
+for i in range(r):
+    for j in range(c):
+        if i == j:
+            A[i][j] = 1
+        else:
+            A[i][j] = 0
+
+# Print the modified matrix
+for i in range(r):
+    for j in range(c):
+        print(A[i][j], end=" ")
+    print()
