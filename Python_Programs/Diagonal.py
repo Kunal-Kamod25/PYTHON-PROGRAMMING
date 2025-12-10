59a634f41d2e981f41a460f05c2344b54c2c1ab2
Kunal Kamod <kunalkamod3@gmail.com>
2025-12-10 20:31:27 +0530

    Create Diagonal.py

---
+++ Python_Programs/Diagonal.py
@@ -0,0 +1,21 @@
+# Initial value but will be overwritten by user input
+n = int(input("Size of matrix: "))
+
+# Create an n x n matrix
+A = [[0] * n for _ in range(n)]
+
+# Input matrix elements
+for i in range(n):
+    for j in range(n):
+        A[i][j] = int(input())
+
+# Print the matrix
+for i in range(n):
+    for j in range(n):
+        print(A[i][j], end=" ")
+    print()
+
+# Print the main diagonal elements
+for i in range(n):
+    print(A[i][i], end="\t")
+print()
