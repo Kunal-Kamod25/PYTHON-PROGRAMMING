91cd3c485b66d3f7fd67eb5ac63c37d2ee7825c2
Kunal Kamod <kunalkamod3@gmail.com>
2025-12-11 21:51:25 +0530

    Checker_board.py

---
+++ Python_Programs/Checker_board.py
@@ -0,0 +1,14 @@
+n = int(input("enter a n: "))
+
+num = 1
+i = 1
+
+while i <= n:
+    for j in range(1, n + 1):
+        if (i + j) % 2 == 0:
+            print(num, end=" ")
+            num += 1
+        else:
+            print("-", end=" ")
+    print()
+    i += 1
