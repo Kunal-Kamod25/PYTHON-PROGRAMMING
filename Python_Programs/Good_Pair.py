7ef27a4d5210a42f313f9cf0e03dd4f5e776e408
Kunal Kamod <kunalkamod3@gmail.com>
2025-12-12 19:45:54 +0530

    Good_Pair.py

---
+++ Python_Programs/Good_Pair.py
@@ -0,0 +1,13 @@
+n = int(input("enter array: "))
+
+print("enter array elements:")
+A = [int(input()) for _ in range(n)]
+
+cnt = 0
+for i in range(n):
+    for j in range(i + 1, n):
+        if A[i] == A[j]:
+            print(f"({i},{j})", end="")
+            cnt += 1
+
+print(f"\nthere are {cnt} good pair:")
