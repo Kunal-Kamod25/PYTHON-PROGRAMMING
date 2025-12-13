5350ec1cacf914f1884a3811ffea42353c8d1e2f
Kunal Kamod <kunalkamod3@gmail.com>
2025-12-13 20:14:18 +0530

    Col_Multiplication.py

---
+++ ForLoop/Col_Multiplication.py
@@ -0,0 +1,13 @@
+n = int(input("enter an n: "))
+
+print("enter matrix:")
+mat = []
+for r in range(n):
+    row = list(map(int, input().split()))
+    mat.append(row)
+
+mul = 1
+for r in range(n):
+    mul *= mat[r][1]   # second column (index 1)
+
+print("multiplication is =", mul)
