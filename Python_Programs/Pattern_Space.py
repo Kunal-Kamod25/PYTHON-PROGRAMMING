13e5073b73a2a8bd8fde09cc4ae77472bc8bfd79
Kunal Kamod <kunalkamod3@gmail.com>
2025-12-15 00:51:59 +0530

    Pattern_Space.py

---
+++ Python_Programs/Pattern_Space.py
@@ -0,0 +1,21 @@
+n = int(input("enter an integer: "))
+
+s = 0
+i = 1
+tn = n
+
+while i <= n:
+    ts = 0
+    while ts < s:
+        print(" ", end="")
+        ts += 1
+
+    j = 1
+    while j <= tn:
+        print(j, end=" ")
+        j += 1
+
+    print()
+    s += 2
+    tn -= 1
+    i += 1
