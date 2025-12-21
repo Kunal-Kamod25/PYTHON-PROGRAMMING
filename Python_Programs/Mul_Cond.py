c0bf19694df5b0ffff30667c5d0db0a58dc41902
Kunal Kamod <kunalkamod3@gmail.com>
2025-12-21 16:24:54 +0530

    Mul_Cond.py

---
+++ Python_Programs/Mul_Cond.py
@@ -0,0 +1,14 @@
+n = int(input("Enter the num: "))
+
+if n < 100:
+    print("small")
+elif n > 100 and n < 200:
+    print("large")
+elif n > 201 and n < 300:
+    print("bigger")
+elif n > 301 and n < 400:
+    print("largest")
+elif n > 400:
+    print("very large")
+else:
+    print("invalid num")
