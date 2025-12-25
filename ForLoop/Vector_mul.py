d1554da63ebd6ae570ddda159dc3ccbb502cb925
Kunal Kamod <kunalkamod3@gmail.com>
2025-12-25 19:29:50 +0530

    Add Vector_mul.py for matrix-vector multiplication
    Implement matrix-vector multiplication in Python.

---
+++ ForLoop/Vector_mul.py
@@ -0,0 +1,23 @@
+n = int(input("Enter the size of matrix: "))
+
+# Initialize matrix, vector, and result
+matrix = []
+vector = []
+ans = [0] * n
+
+print("Enter the elements of matrix:")
+for r in range(n):
+    row = list(map(int, input().split()))
+    matrix.append(row)
+
+print("Enter the elements of vector:")
+vector = list(map(int, input().split()))
+
+# Matrix-vector multiplication
+for r in range(n):
+    for c in range(n):
+        ans[r] += matrix[r][c] * vector[c]
+
+print("Resultant vector:")
+for val in ans:
+    print(val)
