e418232c684c1e6dd923653431a5c67b6435bdb5
Kunal Kamod <kunalkamod3@gmail.com>
2026-01-06 21:47:25 +0530

    Add Arithmetic2B.py for calculating a result

---
+++ Python_Programs/Arithmetic2B.py
@@ -0,0 +1,11 @@
+import math
+
+# Read inputs
+x = int(input("Enter integer x: "))
+y = int(input("Enter integer y: "))
+
+# Calculate result
+result = math.sqrt(2 * x * x + 4 * y + x + 10)
+
+# Print result
+print("Answer is =", result)
