690c1ec91b21f4d80fe3f92739806cd5547648d1
Kunal-Kamod25 <kunalkamod3@gmail.com>
2026-01-11 22:33:13 +0530

    Two Sum Problem

---
+++ LeetCode_Problem/01_Two_sum.py
@@ -0,0 +1,13 @@
+class Solution:
+    def twoSum(self, nums, target):
+        ans = []
+        n = len(nums)
+
+        for i in range(n):
+            for j in range(i + 1, n):
+                if nums[i] + nums[j] == target:
+                    ans.append(i)
+                    ans.append(j)
+                    return ans
+
+        return ans
