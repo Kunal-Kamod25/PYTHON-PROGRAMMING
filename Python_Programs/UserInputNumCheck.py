7c9a9a718e486f48454472f65ff4cf1196148f64
Kunal Kamod <kunalkamod3@gmail.com>
2026-02-17 23:20:16 +0530

    Update UserInputNumCheck.py

--- Python_Programs/UserInputNumCheck.py
+++ Python_Programs/UserInputNumCheck.py
@@ -1,9 +1,10 @@
 Num = int(input("Enter the number : "))
 
 if(Num > 0):
-    print("The Number Is Positive")
+    print("The Number is positive")
 elif(Num == 0):
-    print("The Number is Zero")
+    print("The Number is zero")
 else:
 
-    print("The Number is Negative")
+    print("The Number is negative")
+
