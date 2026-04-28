f5869c40f667e206d8a64108d8e79fc6c13b47b4
Kunal Kamod <kunalkamod3@gmail.com>
2026-04-28 23:41:45 +0530

    Fix formatting of output messages in Numposorneg.py

--- ListTupleDic/Numposorneg.py
+++ ListTupleDic/Numposorneg.py
@@ -3,7 +3,7 @@ num = int(input("Enter The Number: "))
 if num > 0:
     print("The Number Is Positive(+)")
 elif num < 0:
-    print("The Number Is Negative(-)")
+    print("The Number is Negative(-)")
 else:
     print("The Number Is Zero(0)")
 
