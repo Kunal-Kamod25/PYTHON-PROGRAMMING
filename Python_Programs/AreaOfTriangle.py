1b2e4605f96a33597ccb0ddff2999a19433e0f43
Kunal Kamod <kunalkamod3@gmail.com>
2026-08-10 12:36:40 +0530

    Update AreaOfTriangle.py

--- Python_Programs/AreaOfTriangle.py
+++ Python_Programs/AreaOfTriangle.py
@@ -1,4 +1,4 @@
-Height = float(input("Enter the height of the triangle: "))
+Height = float(input("Enter the height of the Triangle: "))
 Base = float(input("Enter the base of the triangle: "))
 
 Area = 0.5 * Base * Height
