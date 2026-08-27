ae25fe8cd22bdfa1d82be8144b30039e8c9aeb7e
Kunal Kamod <kunalkamod3@gmail.com>
2026-08-27 21:38:13 +0530

    Update Function_Large.py

--- Python_Programs/Function_Large.py
+++ Python_Programs/Function_Large.py
@@ -5,5 +5,5 @@ def largest(n1, n2):
         print("No is smaller")
 
 
-n1, n2 = map(int, input("enter the two no: ").split())
+n1, n2 = map(int, input("Enter the two no: ").split())
 largest(n1, n2)
