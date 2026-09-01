dd3dca506636e6f671c255f4880e8f472700b9ad
Kunal Kamod <kunalkamod3@gmail.com>
2026-09-01 21:34:25 +0530

    Fix print statement casing for equality check

--- Python_Programs/ComOp.py
+++ Python_Programs/ComOp.py
@@ -1,6 +1,6 @@
 a = 200
 b = 400
-print(f"Equalto: {a} == {b} = {a == b}")
+print(f"equalto: {a} == {b} = {a == b}")
 print(f"Greater than: {a} > {b} = {a > b}")
 print(f"Less than: {a} < {b} = {a < b}")
 print(f"Less than equal to: {a} <= {b} = {a <= b}")
