e0ddcaed819eecd8e31084bacf073be12d0007bb
Kunal Kamod <kunalkamod3@gmail.com>
2026-09-21 18:07:53 +0530

    Fix formatting in Swapping.py output messages

--- Python_Programs/Swapping.py
+++ Python_Programs/Swapping.py
@@ -1,9 +1,9 @@
 a = int(input("Enter First Number: "))
 b = int(input("Enter Second Number: "))
 
-print("Before swapping: a =", a, "b =", b)
+print("Before Swapping: a =", a, "b =", b)
 temp = a
 a = b
 b = temp
-print("After swapping: a =", a, "b =", b)
+print("After Swapping: a =", a, "b =", b)
 
